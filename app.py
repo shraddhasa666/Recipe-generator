5a61a2fc4b7605a1c9be141db0d24a58fb2fc695
shraddha666 <shraddhasa636@gmail.com>
2025-08-23 17:45:13 +0530

    Add Flask backend with Bootstrap frontend for recipe search

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from pymongo import MongoClient
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS
+CORS(app)
 
 # MongoDB connection
 client = MongoClient("mongodb://localhost:27017/")
 db = client["recipeDB"]
 recipes_collection = db["recipes"]
 
-# Route to get all recipes
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 @app.route("/recipes", methods=["GET"])
 def get_recipes():
     recipes = list(recipes_collection.find({}, {"_id": 0}))
     return jsonify(recipes)
 
-# Route to search recipes by strict ingredients
 @app.route("/search", methods=["GET"])
 def search_recipes():
     ingredients = request.args.get("ingredients")
     if not ingredients:
         return jsonify({"error": "Provide ingredients"}), 400
 
-    # Convert query ingredients to lowercase list
     query_ingredients = [i.strip().lower() for i in ingredients.split(",")]
 
-    # Strict search: recipe must contain ALL ingredients
     recipes = list(recipes_collection.find(
         {"ingredients": {"$all": query_ingredients}},
         {"_id": 0}
